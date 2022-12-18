ad351ac2504d66a62ef28462c2a63209631c88e6
koushik.pathuri <119720431+pathurikoushik@users.noreply.github.com>
2022-12-18 14:55:47 +0530

    #include<stdio.h> int main() {     int a,n,i;     scanf("%d",&a);     for(i=1;i<=12;i++)     {         n=i*a;         printf("%d x %d = %d ",a,i,n);         }         }

---
+++ Multiplication_Table_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ 
+    int a,n,i; 
+    scanf("%d",&a); 
+    for(i=1;i<=12;i++)
+    {
+        n=i*a;
+        printf("%d x %d = %d
+",a,i,n);
+        }
+        }
